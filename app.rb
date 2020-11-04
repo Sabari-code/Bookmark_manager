e1693272c22cdea8df6353ad6b24e20531063f08
your namesabari-code <sabarinathk7@gmail.com>
2020-11-04 13:11:23 +0000

    viewing future test 3

--- app.rb
+++ app.rb
@@ -1,9 +1,22 @@
+
 require 'sinatra/base'
+require './lib/bookmark'
 
 class BookmarkManager < Sinatra::Base
+
   get '/' do
-    'Bookmark Manager'
-  end
+  'Bookmark Manager'
+end
+
+get '/bookmarks' do
+  bookmarks = [
+            "http://www.makersacademy.com",
+            "http://www.destroyallsoftware.com",
+            "http://www.google.com"
+           ]
+
+  bookmarks.join
+end
 
   run! if app_file == $0
 end
